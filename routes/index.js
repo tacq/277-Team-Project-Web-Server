6e0cea312094703de872ace8d8dbd6a9eeb46f74
Fangjia Li <hollandlinus@gmail.com>
2016-05-01 08:22:56 -0700

    /search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/IOS";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    mongo.connect(mongoURL, function () {
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('placeInfo');
+        coll.insertOne({
+                id:"3333",
+            street:"40640 Sutro St",
+            city:"hayward",
+            state:"CA",
+            zipcode:"94538",
+            property:"apartment",
+            rooms:"7",
+            price:"3333.99",
+            phone:"5103313862",
+            email:"479331977@gmail.com",
+            description:"3333"
+            },
+            function (err, place) {
+
+                if (place) {
+                    console.log("mongodb success");
+                } else {
+                    console.log(err);
+                }
+            });
+    });
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
---
+++ routes/search.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/IOS";
+var placeList = [];
+
+function generateUuid() {
+    return Math.random().toString() + Math.random().toString()
+        + Math.random().toString();
+}
+/* GET home page. */
+router.post('/', function (req, res, next) {
+    var searchTxt = req.param("searchTxt");
+    var locationTxt = req.param("locationTxt");
+    var propertyType = req.param("propertyType");
+    console.log(req.getParameter);
+    var response = {};
+
+    mongo.connect(mongoURL, function () {
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('placeInfo');
+        console.log(1);
+        coll.find({
+            property: propertyType
+        }).toArray(
+            function (err, place) {
+
+                if (place) {
+                    console.log("mongodb success");
+                    response.code = "success";
+                    for (var i = 0; i < place.length; i++) {
+                        var placeInfo = {
+                            "id": place[i].id,
+                            "street": place[i].street,
+                            "city": place[i].city,
+                            "state": place[i].state,
+                            "zipcode": place[i].zipcode,
+                            "property": place[i].property,
+                            "rooms": place[i].rooms,
+                            "price": place[i].price,
+                            "phone":place[i].phone,
+                            "email":place[i].email,
+                            "description":place[i].description
+                        };
+                        placeList.push(placeInfo);
+                    }
+                    console.log(placeList);
+                    response.value = placeList;
+                    res.send(response);
+                    placeList=[]
+                } else {
+                    console.log(err);
+                }
+            });
+        console.log("2");
+    });
+
+})
+;
+
+module.exports = router;
